14e5d91a641bd0dc4596bb26a527ab1d21e8a0dc
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-05-18 18:57:31 +0900

    コメント欄に動画、画像投稿

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   mount_uploader :video, VideoUploader
   mount_uploader :image, ImageUploader
