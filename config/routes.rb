4edcff6b74ac65d7392da93fdd06dd42216854d1
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-05-21 15:02:00 +0900

    カテゴリごとに表示可能に

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questions do
     resources :answers, only: :create
-
   end
-  resources :categories
-  resources :answers
 
-  resources :likes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories
+
 end
