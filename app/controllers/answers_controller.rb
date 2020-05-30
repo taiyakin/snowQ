f73351222513691a9960fb542bd38bbaf0493fd9
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-05-30 21:36:06 +0900

    CSS修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,25 +8,22 @@ class AnswersController < ApplicationController
 
   def create
     @answer = Answer.new(answer_params)
-    redirect_to "/questions/#{@answer.question.id}"
     @answer.save
+    redirect_to "/questions/#{@answer.question.id}"
   end
 
   def destroy
     @answer.destroy
     redirect_to "/questions/#{@answer.question.id}"
-
   end
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_answer
       @answer = Answer.find(params[:id])
-
     end
 
-    # Only allow a list of trusted parameters through.
     def answer_params
       params.require(:answer).permit(:text, :video, :image).merge(user_id: current_user.id, question_id: params[:question_id])
     end
