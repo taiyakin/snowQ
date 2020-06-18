a6489a45213906b2480fdf27009719943125ee51
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-06-19 00:26:53 +0900

    地図表示

--- app/assets/javascripts/skiresorts.js
+++ app/assets/javascripts/skiresorts.js
@@ -1,1 +1,11 @@
-$("#send-btn").prop("disabled", false);
+    function initMap() {
+        var latlng = new google.maps.LatLng(gon.lat, gon.lng );//中心の緯度, 経度
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,//ズームの調整
+          center: latlng//上で設定した中心
+        });
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+      }
