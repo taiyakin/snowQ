babaa033f7e537b87d5316081802d5e1357c4170
taiyakin <edih_ikihsoy@yahoo.co.jp>
2020-06-20 00:33:54 +0900

    a

--- app/assets/javascripts/skiresorts.js
+++ app/assets/javascripts/skiresorts.js
@@ -1,11 +1,14 @@
-    function initMap() {
-        var latlng = new google.maps.LatLng(gon.lat, gon.lng );//中心の緯度, 経度
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,//ズームの調整
-          center: latlng//上で設定した中心
-        });
-        var marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
-      }
+function initMap() {
+    var latlng = new google.maps.LatLng(gon.lat, gon.lng );//中心の緯度, 経度
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,//ズームの調整
+      center: latlng//上で設定した中心
+    });
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map
+    });
+  }
+
+
+
